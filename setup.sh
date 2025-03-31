58f74fbc36668ed49fa6ead5b67e5134739ec3f6
Sam Amsavelu <samvelu@us.ibm.com>
2025-03-30 20:07:32 -0500

    Change for RHEL9

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 dnf update -y
-dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-dnf install python39 ansible -y
+dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+dnf install python312 ansible -y
